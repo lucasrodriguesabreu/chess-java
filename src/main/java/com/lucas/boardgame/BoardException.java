93ba41b24891ac3c5fe4a332dfdbc185acee309e
Lucas Rodrigues <lucasrodrigues@MacBook-Air-de-Lucas.local>
2024-11-28 16:53:55 -0300

    BoardException and defensive programming

---
+++ src/main/java/com/lucas/boardgame/BoardException.java
@@ -0,0 +1,13 @@
+package com.lucas.boardgame;
+
+public class BoardException extends RuntimeException{
+
+//    private static final long serialVersionUID = 1l;
+
+    public BoardException(String msg){
+        super(msg);
+    }
+
+
+
+}
